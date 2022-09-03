c6170f2459980a47d1fe93304cecbdccf55dd25d
Kevin Gasca <gasca.kevin.a@gmail.com>
2022-09-03 07:27:27 -0700

    App changes.

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,12 +1,30 @@
+import { useEffect, useState } from 'react';
 
-import Pokemon from './Pokemon';
-import Menu from './Menu';
+import Menu from './components/Menu';
+import Pokemon from './components/Pokemon';
 
 const App = () => {
-    return (<>
-    <Menu />
-    <Pokemon active={9}/>
-    </>)
+    const [trainer, setTrainer] = useState(-1)
+    const [load, setLoad] = useState(<><h1>Loading..</h1></>);
+
+    useEffect(() => {
+        console.log(trainer);
+        if (trainer === -1) {
+        setLoad(<>
+            <button onClick={() => setTrainer(0)}>Kevin</button>
+            <button onClick={() => setTrainer(2)}>Chris</button>
+            <button onClick={() => setTrainer(3)}>Erik</button>
+            <button onClick={() => setTrainer(1)}>Kenneth</button>
+        </>);
+        } else {
+        setLoad(<>
+            <Menu trainer={trainer}/>
+            <Pokemon trainer={trainer} active={8}/>
+        </>)
+        }
+    }, [trainer]);
+
+    return (load)
 }
 
 export default App
